c56fb160ae9bb6c3ff59c5f11c7ddc914a8f85ef
wolfycodes4fun <amethacodes@gmail.com>
2026-06-24 00:07:07 +0530

    add Person service

--- src/main/java/com/example/demo/dao/FakePersonDataAccessService.java
+++ src/main/java/com/example/demo/dao/FakePersonDataAccessService.java
@@ -1,13 +1,18 @@
 package com.example.demo.dao;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 import com.example.demo.model.Person;
 
 public class FakePersonDataAccessService implements PersonDao {
 
+    private static List<Person> DB = new ArrayList<>();
+
     @Override
     public int insertPerson(UUID id, Person person) {
-        return 0;
+        DB.add(new Person(id, person.getFirstName(), person.getLastName()));
+        return 1;
     }
 }
