90ac60449c74f832f52afa70780dddcc628857f6
wolfycodes4fun <amethacodes@gmail.com>
2026-06-24 12:04:55 +0530

    add endpoints to add and retrieve all users from temp DB

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 A quick go through on the workings/architecture on setting up a Springboot project and how they work.
 
-Initially 3 layers are presented:
+Initially 3 layers are presented:  
     1) API Layer/ Controller Layer: Defines routes and request types (ex- GET, POST...) & more  
     2) Service Layer: Where the business logic sits  
     3) Data Access Layer  
--- src/main/java/com/example/demo/service/PersonService.java
+++ src/main/java/com/example/demo/service/PersonService.java
@@ -1,5 +1,7 @@
 package com.example.demo.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
@@ -18,5 +20,9 @@ public class PersonService {
 
     public int addPerson(Person person) {
         return personDao.insertPerson(person);
+    }
+
+    public List<Person> getAllPeople() {
+        return personDao.selectAllPeople();
     }
 }
